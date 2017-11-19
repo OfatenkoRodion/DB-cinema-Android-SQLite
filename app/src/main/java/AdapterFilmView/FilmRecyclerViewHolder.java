f75dca0ba4b0b426123b27650ac97c9241b1b7d5
Rodion <madnessjust4@gmail.com>
2017-11-19 16:49:22 +0300

    Added RecyclerItemClickListener

--- app/src/main/java/AdapterFilmView/FilmRecyclerViewHolder.java
+++ app/src/main/java/AdapterFilmView/FilmRecyclerViewHolder.java
@@ -1,15 +1,18 @@
 package AdapterFilmView;
 
 
+import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import Entity.Film;
 import best.the.rodionofatenko.com.clientfortestingcodequality.R;
+import best.the.rodionofatenko.com.clientfortestingcodequality.RecyclerItemClickListener;
 
 public class FilmRecyclerViewHolder extends RecyclerView.ViewHolder
 {
@@ -19,10 +22,12 @@ public class FilmRecyclerViewHolder extends RecyclerView.ViewHolder
     private TimeRecylerAdapter adapter;
     private TextView name;
     private TextView description;
+    private Context ctx;
 
     public FilmRecyclerViewHolder(View itemView)
     {
         super(itemView);
+        ctx=itemView.getContext();
         recyclerView =(RecyclerView) itemView.findViewById(R.id.recyclerTimes);
         name=(TextView) itemView.findViewById(R.id.textViewFilmName);
         description=(TextView) itemView.findViewById(R.id.textViewDescription);
@@ -31,13 +36,25 @@ public class FilmRecyclerViewHolder extends RecyclerView.ViewHolder
 
         adapter=new TimeRecylerAdapter();
         recyclerView.setAdapter(adapter);
+
     }
 
-    public void bind(ArrayList<String> times, Film film)
+    public void bind(final ArrayList<String> times, Film film)
     {
         this.name.setText(film.getName());
         this.description.setText(film.getDescription());
         adapter.addAll(times);
         adapter.notifyItemChanged(times.size());
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(itemView.getContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        Toast.makeText(ctx,"onItemClick "+times.get(position),Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                        Toast.makeText(ctx,"onLongItemClick "+times.get(position),Toast.LENGTH_LONG).show();
+                    }
+                })
+        );
     }
 }
