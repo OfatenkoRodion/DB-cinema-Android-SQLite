8613c91bdf65ae026b75778ff19072c1f1fb9cea
Rodion <madnessjust4@gmail.com>
2017-11-24 23:37:35 +0300

    Data for test, looked some bug

--- app/src/main/java/best/the/rodionofatenko/com/clientfortestingcodequality/BasicActivity.java
+++ app/src/main/java/best/the/rodionofatenko/com/clientfortestingcodequality/BasicActivity.java
@@ -11,6 +11,7 @@ public class BasicActivity extends AppCompatActivity implements View.OnClickList
 {
     Button adminPanel, userPanel;
     Button clearDB;
+    Button testDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,9 +21,11 @@ public class BasicActivity extends AppCompatActivity implements View.OnClickList
         adminPanel=(Button) findViewById(R.id.adminPanelButton);
         userPanel=(Button) findViewById(R.id.userPanelButton);
         clearDB=(Button) findViewById(R.id.clearButton);
+        testDB=(Button) findViewById(R.id.testButton);
         adminPanel.setOnClickListener(this);
         userPanel.setOnClickListener(this);
         clearDB.setOnClickListener(this);
+        testDB.setOnClickListener(this);
     }
 
     @Override
@@ -42,12 +45,80 @@ public class BasicActivity extends AppCompatActivity implements View.OnClickList
         }
         if (view == clearDB)
         {
-            try{
-           DB_Cinema db_cinema = new DB_Cinema(this);
-            db_cinema.clear();}
-            catch (Exception e){
-                Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+            DB_Cinema db_cinema = new DB_Cinema(this);
+            db_cinema.clear();
+
+        }
+        if (view == testDB)
+        {
+            DB_Cinema db_cinema = new DB_Cinema(this);
+            db_cinema.clear();
+
+            db_cinema.addFilm("Назад в будущее","«Назад в будущее» — это история о подростке, который случайно попал из 1985 года в 1955 год. Он встречается со своими родителями, учащимися средней школы, и случайно пробуждает к себе романтический интерес со стороны своей матери. Марти должен исправить вызванные им нарушения в ходе истории, заставив своих родителей полюбить друг друга, а также найти способ вернуться обратно в 1985 год.");
+            db_cinema.addFilm("Терминатор","В центре сюжета — противостояние живого солдата и киборга-терминатора, прибывших в 1984 год из постапокалиптического 2029 года. Цель терминатора: убить Сару Коннор — девушку, чей ещё нерождённый сын выиграет войну человечества с машинами. Влюблённый в Сару солдат Кайл Риз пытается помешать терминатору. В фильме поднимаются проблемы путешествий во времени, судьбы, создания искусственного интеллекта, поведения людей в экстремальных ситуациях.");
+            db_cinema.addFilm("Парк юрского периода","В фильме рассказывается о вымышленном острове, где учёные создали парк развлечений с клонированными динозаврами. Джон Хэммонд (Ричард Аттенборо) приглашает группу учёных, сыгранную Сэмом Ниллом, Джеффом Голдблюмом и Лорой Дерн, чтобы они проинспектировали парк до его открытия. Из-за диверсии, устроенной одним из сотрудников, динозавры прорываются на волю, а технический персонал и посетители пытаются спастись.");
+            db_cinema.addFilm("Годзилла 1998г.","Действие переносится в 1998 год и рассказывает о том, как несколько японских рыбацких судов в южной части Тихого Океана, неподалёку от островов Французской Полинезии, подверглись нападению загадочного хищника. Эти события привлекают внимание правительств США и Франции. Чтобы разобраться, что это за чудовище, к расследованию привлекли сотрудника комитета ядерного контроля доктора Ника Татопулоса, который до этого 3 года исследовал гигантских дождевых червей-мутантов в окрестностях Чернобыля.");
+
+            db_cinema.addPlaceCategory("vip");
+            db_cinema.addPlaceCategory("good");
+
+            db_cinema.addHall(1,100);
+            for (int i=1;i<=10;i++)
+            {
+                db_cinema.addRow(i,1,1,10);
+                for (int j=1;j<=10;j++)
+                {
+                    db_cinema.addPlace(j,i);
+                }
             }
+
+
+            db_cinema.addHall(2,110);
+            for (int i=11;i<=20;i++)
+            {
+                db_cinema.addRow(i-10,2,1,10);
+                for (int j=1;j<=11;j++)
+                {
+                    db_cinema.addPlace(j,i);
+                }
+            }
+
+            db_cinema.addHall(3,80);
+            for (int i=21;i<=29;i++)
+            {
+                db_cinema.addRow(i-21,3,1,10);
+                for (int j=1;j<=10;j++)
+                {
+                    db_cinema.addPlace(j,i);
+                }
+            }
+
+            db_cinema.addSession("24.11.2017","13:15",3,1);
+            db_cinema.addSession("24.11.2017","18:00",1,1);
+            db_cinema.addSession("24.11.2017","23:00",1,1);
+            db_cinema.addSession("24.11.2017","8:00",2,2);
+            db_cinema.addSession("24.11.2017","13:45",2,2);
+            db_cinema.addSession("24.11.2017","22:00",2,2);
+            db_cinema.addSession("24.11.2017","17:30",2,3);
+
+            db_cinema.addSession("25.11.2017","14:35",2,3);
+            db_cinema.addSession("25.11.2017","17:35",2,3);
+            db_cinema.addSession("25.11.2017","11:20",3,1);
+            db_cinema.addSession("25.11.2017","13:15",3,1);
+            db_cinema.addSession("25.11.2017","18:00",1,1);
+            db_cinema.addSession("25.11.2017","23:00",1,1);
+            db_cinema.addSession("25.11.2017","15:50",2,2);
+
+            db_cinema.addSession("26.11.2017","15:50",2,2);
+            db_cinema.addSession("26.11.2017","17:55",2,2);
+            db_cinema.addSession("26.11.2017","13:15",3,1);
+            db_cinema.addSession("26.11.2017","18:00",1,1);
+            db_cinema.addSession("26.11.2017","23:00",1,1);
+            db_cinema.addSession("26.11.2017","11:00",2,4);
+            db_cinema.addSession("26.11.2017","13:00",2,4);
+            db_cinema.addSession("26.11.2017","16:30",1,4);
+            db_cinema.addSession("26.11.2017","22:55",3,3);
+
         }
     }
 }
